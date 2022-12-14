594d026bb94ed4e0e97b80849676cf81814b0b27
howabouthis <>
2022-12-14 09:56:38 +0200

    Stop nvchad ui from interfering with notifications

--- lua/nvchad_ui/lsp.lua
+++ lua/nvchad_ui/lsp.lua
@@ -17,26 +17,27 @@ vim.diagnostic.config {
   update_in_insert = false,
 }
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = "single",
-})
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = "single",
-  focusable = false,
-  relative = "cursor",
-})
+-- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+--   border = "single",
+-- })
+--
+-- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+--   border = "single",
+--   focusable = false,
+--   relative = "cursor",
+-- })
 
 -- suppress error messages from lang servers
-vim.notify = function(msg, log_level)
-  if msg:match "exit code" then
-    return
-  end
-  if log_level == vim.log.levels.ERROR then
-    vim.api.nvim_err_writeln(msg)
-  else
-    vim.api.nvim_echo({ { msg } }, true, {})
-  end
-end
+-- vim.notify = function(msg, log_level)
+--   if msg:match "exit code" then
+--     return
+--   end
+--   if log_level == vim.log.levels.ERROR then
+--     vim.api.nvim_err_writeln(msg)
+--   else
+--     vim.api.nvim_echo({ { msg } }, true, {})
+--   end
+-- end
 
 -- Borders for LspInfo winodw
 local win = require "lspconfig.ui.windows"
